137696a24e49ce410454c187c13e27250b43be0e
Oliver <au61847@uni.au.dk>
2021-10-13 11:02:09 +0200

    added chan

--- Chan.py
+++ Chan.py
@@ -9,16 +9,12 @@ def uh_with_size(points, h):
         hulls[i] = GrahamsScan.hall(le, partition)
     uh = []
     p = min(points, key=lambda x: x[0])
-    p_max = max(points, key=lambda x: x[0]
+    p_max = max(points, key=lambda x: x[0])
     for i in range(h):
         uh.append(p)
         if points[i] == p_max:
             break
         t = 0
-
-
-
-
         for j in range(len(partitions[i])-1):
             slope = (partitions[i][j][1]-p[1])/(partitions[i][j][0]-p[0])
             if slope > t:
