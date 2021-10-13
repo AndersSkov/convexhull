5c2d7cf78d779f4fee4c165b2cb6da0a2b6c4828
Oliver <au61847@uni.au.dk>
2021-10-13 12:08:52 +0200

    added chan

--- Chan.py
+++ Chan.py
@@ -13,7 +13,7 @@ def uh_with_size(points, h):
     partitions = np.array_split(points, h)
     hulls = []
     for i, partition in enumerate(partitions):
-        hulls[i] = GrahamsScan.hall(le, partition)
+        hulls.append(GrahamsScan.hall(le, partition))
     uh = []
     p = min(points, key=lambda x: x[0])
     p_max = max(points, key=lambda x: x[0])
